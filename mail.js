11ed32d63dbf3c5811317d8f5b4647b730e41aaa
SoldzToys <43523244+SoldzToys@users.noreply.github.com>
2018-10-02 22:14:35 -0400

    Update mail.js

--- mail.js
+++ mail.js
@@ -12,17 +12,17 @@ client.user.setActivity('with her +help string toy');
 client.on('message', message => {
   
     if (message.content === `${prefix}hello`) {
-    message.reply(`Meow.`);
+    message.channel.send(`Meow.`);
    }
    
      if (message.content === `${prefix}help`) {
-    message.reply(`+hello, +meow, +cat`)
+    message.channel.send(`+hello, +meow, +cat`)
     message.react("🐱");
     
   }
   
     if (message.content === `${prefix}meow`) {
-    message.reply(`Bark!`);
+    message.channel.send(`Bark!`);
       
     }
     
