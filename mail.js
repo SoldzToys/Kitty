7dd01d55846dc086fd7051a354abe4dcb775cb64
SoldzToys <43523244+SoldzToys@users.noreply.github.com>
2018-10-14 03:32:16 -0400

    Update mail.js

--- mail.js
+++ mail.js
@@ -73,14 +73,15 @@ client.on('guildDelete', guild => {
   return channel.send(embed);
 });     
 
-client.on("guildUpdate", function (oldGuild, newGuild) {
+client.on("guildUpdate", function guild (oldGuild, newGuild, guild) {
          let channel = client.channels.get("499832353544470539");
 	
         const eeembed = new Discord.RichEmbed()
             .setColor("#7289da")
             .setAuthor(`A Guild Has been Updated`)
-            .addField(`oldGuild:`, `${oldGuild}`)
-            .addField(`newGuild:`, `${newGuild}`)
+            .addField(`Old Guild Name:`, `${oldGuild}`)
+            .addField(`New Guild Name:`, `${newGuild}`)
+	    .addField("ID", guild.id, true)
         return channel.send(eeembed);
 });
 	
