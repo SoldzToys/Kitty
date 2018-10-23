9e34dc31e85265b33385279a7e37f3d3d285d597
SoldzToys <43523244+SoldzToys@users.noreply.github.com>
2018-10-23 13:47:02 -0400

    Update mail.js

--- mail.js
+++ mail.js
@@ -107,9 +107,9 @@ client.on('guildDelete', guild => {
   return channel.send(embed);
 });     
 
-client.on("guildUpdate", function (oldGuild, newGuild, guild) {
+client.on("guildUpdate", function (oldGuild, newGuild) {
          let channel = client.channels.get("499832353544470539");
-	 let gicon = newGuild.guild.iconURL;
+	 let gicon = newGuild.iconURL;
         const eeembed = new Discord.RichEmbed()
             .setColor("#7289da")
             .setThumbnail(gicon)
