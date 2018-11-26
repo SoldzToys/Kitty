ec759ef1fcfbcafa9869f34c5190e01ccadad3f9
SoldzToys <43523244+SoldzToys@users.noreply.github.com>
2018-11-25 20:14:22 -0500

    Update mail.js

--- mail.js
+++ mail.js
@@ -115,10 +115,10 @@ message.channel.send(catembed);
 	
      if (message.content.startsWith(`${prefix}panda`)) { 
       let { body } = await request.get(`https://api-to.get-a.life/pandaimg`); 
-      let { body } = await request.get('https://some-random-api.ml/pandafact')
+      let { body2 } = await request.get('https://some-random-api.ml/pandafact')
     let pandaembed = new Discord.RichEmbed()
     .setTitle('Panda 🐼')
-    .setDescription(body.fact) 
+    .setDescription(body2.fact) 
     .setColor("#7289da")
     .setImage(body.link);
     message.channel.send(pandaembed);
