491327d5c3c62f3cb5aa1444fafa370cb48493b4
SoldzToys <43523244+SoldzToys@users.noreply.github.com>
2018-11-25 20:32:27 -0500

    Update mail.js

--- mail.js
+++ mail.js
@@ -115,7 +115,7 @@ message.channel.send(catembed);
 	
      if (message.content.startsWith(`${prefix}panda`)) { 
   //  let { body } = await request.get('https://some-random-api.ml/pandafact')
-     let { body } = await request.get('https://api-to.get-a.life/pandafact', 'https://api-to.get-a.life/pandaimg'); 
+     let { body } = await request.get('https://api-to.get-a.life/pandafact' + 'https://api-to.get-a.life/pandaimg'); 
      //let { body }  = await request.get('https://some-random-api.ml/pandafact')
     let pandaembed = new Discord.RichEmbed()
     .setTitle('Panda 🐼')
